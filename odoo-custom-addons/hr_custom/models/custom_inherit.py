0146b1da2ad8d32cee7559e4c105bef1fd130fde
Duong <vovanduong258@gmail.com>
2022-10-31 08:52:31 +0700

    update hr custom

--- odoo-custom-addons/hr_custom/models/custom_inherit.py
+++ odoo-custom-addons/hr_custom/models/custom_inherit.py
@@ -19,6 +19,39 @@ class CustomInherit(models.Model):
         ('2', 'Verygood'), ('3', 'Excellent')],
         'Appreciation', required=True, default='1')
 
-    #create date for employee
+    # create datetime for employee
     created_on = fields.Datetime(string = "Created on", required=True)
     
+    # insert field for company technology
+    joining_day = fields.Datetime(string = "Joining Day")
+    project = fields.Char(string = "Project")
+    program = fields.Char(string = "Program")
+    year_of_experience = fields.Char(string = "Years of Exeperience")
+    before_company = fields.Char(string = "Before Company")
+    year_at_company = fields.Char(string = "Years at Company")
+
+    # insert field for contact information
+    current_address = fields.Char(string = "Current Address")
+    permanent_address = fields.Char(string = "Permanent Address")
+    private_email = fields.Char(string = "Private Email")
+    personal_phone = fields.Char(string = "Personal Phone")
+    skype = fields.Char(string = "Skype")
+
+    # insert field for bank information
+    bank_account = fields.Char(string = "Bank Account")
+    number = fields.Char(string = "Number")
+    bank_name = fields.Char(string = "Bank Name")
+    bank_branch = fields.Char(string = "Bank Branch")
+    bank_location = fields.Char(string = "Bank Location")
+
+    #insert field for citizenship
+    identification_issue_date = fields.Date(string = "Identification Issue Date")
+    identification_issued = fields.Char(string = "Identification Issued By Police Department Of")
+    passport_expired_date = fields.Date(string = "Passport Expired Date")
+    sin_no = fields.Char(string = "SIN No")
+    pit_code = fields.Char(string = "PIT code")
+
+
+    #insert field for vehicle
+    vehicle_number = fields.Char(string = "Vehicle Number")
+    vehicle_type = fields.Char(string = "Vehicle Type")
